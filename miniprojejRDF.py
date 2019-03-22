3bec89408cc058dca137ebcea1ea84617776acbb
Jesse Ferreira Filho <jessefilhojfnf@gmail.com>
2019-03-22 14:30:52 +0100

    write the rdf file with query

--- miniprojejRDF.py
+++ miniprojejRDF.py
@@ -3,8 +3,30 @@ import rdflib
 
 g=rdflib.Graph()
 #g.load('http://dbpedia.org/resource/Semantic_Web')
-g.load('human.rdf')
+#g.load('human.rdf')
+g.parse('human.rdf')
 
 
-for s,p,o in g:
-    print s,p,o
+#for s,p,o in g:
+#    print s,p,o
+
+qres = g.query(
+"""
+PREFIX humans: <http://www.inria.fr/2007/09/11/humans.rdfs#>
+PREFIX xsd:    <http://www.w3.org/2001/XMLSchema#>
+	
+SELECT ?name ?pointure ?shirtsize ?age ?shoesize ?trouserssize
+WHERE {
+?x humans:name ?name .
+?x humans:shirtsize ?shirtsize .
+?x humans:age ?age .
+?x humans:shoesize ?shoesize .
+?x humans:trouserssize ?trouserssize .
+
+FILTER (xsd:string(?name) = "John" )
+}
+""")
+
+
+for row in qres:
+    print("Name: %s Pointure: %s shirtsize:%s Age:%s shoesize:%s trouserssize:%s" % row)
