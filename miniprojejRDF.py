7e019265848b93a1936b1a0cfc24332b17d407bf
Jesse F Nascimento Filho <jessefilhojfnf@gmail.com>
2019-03-23 20:59:47 +0100

    added list of to do task on top of file

--- miniprojejRDF.py
+++ miniprojejRDF.py
@@ -1,6 +1,17 @@
 import rdflib
 
 
+#Goals of this script
+
+#1 - programme simple comment exploiter	par	programme un jeu de	données RDF	et RDFS
+#2 - ouvrir [DONE]
+#3 - modifier [TO DO]
+#4 - sauvegarder [TO DO]
+#5 - rechercher	des	parties [DOING]
+#6 - les exploiter d’une manière ou	d’une autre	et sauvegarder les résultats obtenus [DOING]
+#7 - tester	des	requêtes SPARQL, sans schéma en	RDFS [TO DO]
+#8 - tester	des	requêtes SPARQL, avec schéma en	RDFS [TO DO]
+
 g=rdflib.Graph()
 #g.load('http://dbpedia.org/resource/Semantic_Web')
 #g.load('human.rdf')
@@ -43,5 +54,35 @@ qres = g.query(
     }
     """)
 
+# for row in qres:
+#     print(row)
+
+
+'http://www.inria.fr/2007/09/11/humans.rdfs#trouserssize'
+'http://www.inria.fr/2007/09/11/humans.rdfs#shirtsize'
+'http://www.inria.fr/2007/09/11/humans.rdfs#name'
+'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+'http://www.inria.fr/2007/09/11/humans.rdfs#hasChild'
+'http://www.inria.fr/2007/09/11/humans.rdfs#shoesize'
+'http://www.inria.fr/2007/09/11/humans.rdfs#hasSpouse'
+'http://www.inria.fr/2007/09/11/humans.rdfs#hasParent'
+'http://www.inria.fr/2007/09/11/humans.rdfs#hasFriend'
+'http://www.inria.fr/2007/09/11/humans.rdfs#hasFather'
+'http://www.inria.fr/2007/09/11/humans.rdfs#age'
+'http://www.inria.fr/2007/09/11/humans.rdfs#hasMother'
+
+qres = g.query(
+    """
+    PREFIX humans: <http://www.inria.fr/2007/09/11/humans.rdfs#>
+    PREFIX xsd:    <http://www.w3.org/2001/XMLSchema#>
+
+    SELECT ?name ?friend
+    WHERE {
+    ?name humans:hasFriend ?friend
+    
+    }
+    """)
+
 for row in qres:
-    print(row)
+    print('name:%s friend:%s' % row)
+
